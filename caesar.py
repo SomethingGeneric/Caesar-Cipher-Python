95d9a6cca161c9cd3f923b6df5816b6e4d00310b
Matt C <matthew.compton@mymail.champlain.edu>
2022-09-27 16:32:56 +0000

    multienc and help

--- caesar.py
+++ caesar.py
@@ -2,7 +2,7 @@ import sys
 
 from nostril import nonsense
 
-def Encryption(plaintext, key_val):
+def c_encrypt(plaintext, key_val):
     ciphertext = ''
     for i in range(len(plaintext)):
         special = plaintext[i]
@@ -15,7 +15,7 @@ def Encryption(plaintext, key_val):
     return ciphertext
 
 
-def Decryption(ciphertext, key_val):
+def c_decrypt(ciphertext, key_val):
     plaintext = ''
     for i in range(len(ciphertext)):
         special = ciphertext[i]
@@ -28,14 +28,39 @@ def Decryption(ciphertext, key_val):
 
 def BruteDecrypt(ciphertext):
     for i in range(1,10000):
-        text = Decryption(ciphertext,i)
+        text = c_decrypt(ciphertext,i)
         if not nonsense(text):
             print("Maybe: " + text)
             break
 
+def multi_cypher(plaintext, key_sequence):
+    this_pass = plaintext
+    shifts = [*key_sequence]
+    #print(str(shifts))
+    for shift in shifts:
+        this_pass = c_encrypt(this_pass, int(shift))
+    return this_pass
+
+def multi_cypher_decrypt(starter, key_sequence):
+    this_pass = starter
+    shifts = [*key_sequence]
+    shifts.reverse()
+    #print(str(shifts))
+    for shift in shifts:
+        this_pass = c_decrypt(this_pass, int(shift))
+    return this_pass
+
+if len(sys.argv) == 1:
+    print("Usage:\n- enc <text> <shift>\n- dec <text> <shift>\n- multienc <text> <shift_seq>\n- multidec <text> <shift_seq>\n- magicdec <text>")
+    sys.exit()
+
 if sys.argv[1] == "enc":
-    print(Encryption(sys.argv[2], int(sys.argv[3])))
+    print(c_encrypt(sys.argv[2], int(sys.argv[3])))
 elif sys.argv[1] == "dec":
-    print(Decryption(sys.argv[2], int(sys.argv[3])))
-elif sys.argv[1] == "magic":
+    print(c_decrypt(sys.argv[2], int(sys.argv[3])))
+elif sys.argv[1] == "multienc":
+    print(multi_cypher(sys.argv[2], sys.argv[3]))
+elif sys.argv[1] == "multidec":
+    print(multi_cypher_decrypt(sys.argv[2], sys.argv[3]))
+elif sys.argv[1] == "magicdec":
     BruteDecrypt(sys.argv[2])
